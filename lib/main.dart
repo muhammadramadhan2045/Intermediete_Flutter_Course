5f0bc657acb5afa0ed0a0384f399ca69719d5c8a
Muhammad Ramadhan <muhammadramadhan2045@gmail.com>
2024-04-23 11:22:01 +0700

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:latihan_image_picker/provider/home_provider.dart';
+import 'package:latihan_image_picker/screen/home_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<HomeProvider>(
+      create: (context) => HomeProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomeScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:latihan_image_picker/provider/home_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:image_picker/image_picker.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Camera Project"),
+        actions: [
+          IconButton(
+            onPressed: () => _onUpload(),
+            icon: const Icon(Icons.upload),
+            tooltip: "Unggah",
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 3,
+              child: context.watch<HomeProvider>().imagePath == null
+                  ? const Align(
+                      alignment: Alignment.center,
+                      child: Icon(
+                        Icons.image,
+                        size: 100,
+                      ),
+                    )
+                  : _showImage(),
+            ),
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => _onGalleryView(),
+                    child: const Text("Gallery"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => _onCameraView(),
+                    child: const Text("Camera"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => _onCustomCameraView(),
+                    child: const Text("Custom Camera"),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _onUpload() async {}
+
+  _onGalleryView() async {}
+
+  _onCameraView() async {}
+
+  _onCustomCameraView() async {}
+
+  Widget _showImage() {
+    return Container();
+  }
+}
