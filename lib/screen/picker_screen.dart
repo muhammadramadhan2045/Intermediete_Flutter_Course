965d7742d67349ce292b208286d77e7bec289f90
Muhammad Ramadhan <muhammadramadhan2045@gmail.com>
2024-04-30 11:05:47 +0700

    done

--- lib/screen/picker_screen.dart
+++ lib/screen/picker_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'package:geocoding/geocoding.dart' as geo;
 
 class PickerScreen extends StatefulWidget {
   const PickerScreen({super.key});
@@ -8,17 +11,141 @@ class PickerScreen extends StatefulWidget {
 }
 
 class _PickerScreenState extends State<PickerScreen> {
+  final dicodingOffice = const LatLng(-6.8957473, 107.6337669);
+  late GoogleMapController mapController;
+  late final Set<Marker> markers = {};
+
+  geo.Placemark? placemark;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Picker Screen'),
       ),
-      body: const Center(
-        child: Text(
-          'Picker Screen',
+      body: Center(
+        child: Stack(
+          children: [
+            GoogleMap(
+              onLongPress: (LatLng latLng) {
+                onLongPressGoogleMap(latLng);
+              },
+              onMapCreated: (controller) async {
+                final info = await geo.placemarkFromCoordinates(
+                    dicodingOffice.latitude, dicodingOffice.longitude);
+                final place = info[0];
+                final street = place.street!;
+                final address =
+                    '${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}';
+                setState(() {
+                  placemark = place;
+                });
+                defineMarker(dicodingOffice, street, address);
+                final marker = Marker(
+                  markerId: const MarkerId("source"),
+                  position: dicodingOffice,
+                );
+                setState(() {
+                  mapController = controller;
+                  markers.add(marker);
+                });
+              },
+              initialCameraPosition: CameraPosition(
+                zoom: 18,
+                target: dicodingOffice,
+              ),
+              markers: markers,
+              zoomControlsEnabled: false,
+              mapToolbarEnabled: false,
+              myLocationButtonEnabled: false,
+            ),
+            Positioned(
+              top: 16,
+              right: 16,
+              child: FloatingActionButton(
+                child: const Icon(Icons.my_location),
+                onPressed: () async => onMyLocationButtonPress(),
+              ),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  void defineMarker(LatLng latLng, String street, String address) {
+    final marker = Marker(
+      markerId: const MarkerId("source"),
+      position: latLng,
+      infoWindow: InfoWindow(
+        title: street,
+        snippet: address,
+      ),
+    );
+    setState(() {
+      markers.clear();
+      markers.add(marker);
+    });
+  }
+
+  void onLongPressGoogleMap(LatLng latLng) async {
+    debugPrint("Latitude: ${latLng.latitude}");
+    final info =
+        await geo.placemarkFromCoordinates(latLng.latitude, latLng.longitude);
+    final place = info[0];
+    final street = place.street!;
+    final address =
+        '${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}';
+    setState(() {
+      placemark = place;
+    });
+    defineMarker(latLng, street, address);
+    mapController.animateCamera(
+      CameraUpdate.newLatLng(latLng),
+    );
+  }
+
+  void onMyLocationButtonPress() async {
+    final Location location = Location();
+    late bool serviceEnabled;
+    late PermissionStatus permissionGranted;
+    late LocationData locationData;
+
+    locationData = await location.getLocation();
+    final latLng = LatLng(locationData.latitude!, locationData.longitude!);
+
+    final info =
+        await geo.placemarkFromCoordinates(latLng.latitude, latLng.longitude);
+
+    final place = info[0];
+    final street = place.street!;
+    final address =
+        '${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}';
+    setState(() {
+      placemark = place;
+    });
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        print("Location services is not available");
+        return;
+      }
+    }
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        print("Location permission is denied");
+        return;
+      }
+    }
+
+    locationData = await location.getLocation();
+    // final latLng = LatLng(locationData.latitude!, locationData.longitude!);
+    defineMarker(latLng, street, address);
+    mapController.animateCamera(
+      CameraUpdate.newLatLng(latLng),
     );
   }
 }
